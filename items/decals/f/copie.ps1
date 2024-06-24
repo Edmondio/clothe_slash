be459c2a788b23f32d50bd507a85baa6b5a581d8
Edmondio <83596559+Edmondio@users.noreply.github.com>
2024-06-24 13:35:41 +0200

    update for decals

---
+++ items/decals/f/copie.ps1
@@ -0,0 +1,19 @@
+# Demander à l'utilisateur combien de copies il souhaite
+$nombreCopies = Read-Host "Combien de copies voulez-vous?"
+
+# Définir le nom de fichier source
+$sourceFile = "47.png"
+
+# Vérifier si le fichier source existe
+if (-Not (Test-Path $sourceFile)) {
+    Write-Host "Le fichier source '$sourceFile' n'existe pas." -ForegroundColor Red
+    exit
+}
+
+# Boucle pour copier le fichier
+for ($i = 1; $i -le $nombreCopies; $i++) {
+    $destinationFile = "$i" + "_.png"
+    Copy-Item $sourceFile -Destination $destinationFile
+}
+
+Write-Host "Copie terminée !"
